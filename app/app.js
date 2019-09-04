55aa3f618c7bb80691ee0bc29114b3b23963173a
Andrew Obiano <andrew.obiano@gmail.com>
2019-09-03 19:52:21 -1000

    Second commit

---
+++ app/app.js
@@ -0,0 +1,20 @@
+const members = [
+  "Andrew",
+  "Arthur",
+  "Brian",
+  "Calvin",
+  "Daniel",
+  "David",
+  "Janelle",
+  "Justin",
+  "Keith",
+  "Kevin",
+  "Lewis",
+  "Phi",
+  "Rian",
+  "Rina"
+];
+
+let teamLength = 3;
+let random = Math.floor(Math.random() * teamLength);
+console.log(random);
